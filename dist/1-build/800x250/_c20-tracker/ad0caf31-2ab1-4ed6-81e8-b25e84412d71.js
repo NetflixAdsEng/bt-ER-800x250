3036466c5b235e800ec5db53bd9b82fb7939da9f
Derek Miranda <derek.miranda@ff0000.com>
2020-01-13 17:21:31 -0800

    Disables ribbon only if explicit false

--- dist/1-build/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
+++ dist/1-build/800x250/_c20-tracker/ad0caf31-2ab1-4ed6-81e8-b25e84412d71.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1578944269245,
+  "updated": 1578964881210,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-800x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-800x250",
